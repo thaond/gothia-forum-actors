4890a345a84bd0b94444d91c29971778fab652e1
simon.goransson@gmail.com <simon.goransson@gmail.com@cd2f1761-dfa1-d956-9279-670b467f8114>
2011-11-09 10:44:20 +0000

    updated database information in server.xml

--- trunk/core-bc/composites/svc/src/main/java/se/gothiaforum/util/FriendlyURLNormalizer.java
+++ trunk/core-bc/composites/svc/src/main/java/se/gothiaforum/util/FriendlyURLNormalizer.java
@@ -1,14 +1,19 @@
 /**
- * Copyright (c) 2000-2011 Liferay, Inc. All rights reserved.
+ * Copyright 2010 Västra Götalandsregionen
  *
- * The contents of this file are subject to the terms of the Liferay Enterprise
- * Subscription License ("License"). You may not use this file except in
- * compliance with the License. You can obtain a copy of the License by
- * contacting Liferay, Inc. See the License for the specific language governing
- * permissions and limitations under the License, including but not limited to
- * distribution rights of the Software.
+ *   This library is free software; you can redistribute it and/or modify
+ *   it under the terms of version 2.1 of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation.
  *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
  *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the
+ *   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ *   Boston, MA 02111-1307  USA
  *
  */
 
--- trunk/core-bc/composites/svc/src/test/java/se/gothiaforum/actorarticle/service/TestActroArticleService.java
+++ trunk/core-bc/composites/svc/src/test/java/se/gothiaforum/actorarticle/service/TestActroArticleService.java
@@ -1,14 +1,44 @@
 /**
+ * Copyright 2010 Västra Götalandsregionen
+ *
+ *   This library is free software; you can redistribute it and/or modify
+ *   it under the terms of version 2.1 of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the
+ *   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ *   Boston, MA 02111-1307  USA
+ *
+ */
+
+/**
  * 
  */
 package se.gothiaforum.actorarticle.service;
+
+import static org.junit.Assert.assertEquals;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.junit.Test;
 
 /**
  * @author simongoransson
- *
+ * 
  */
 public class TestActroArticleService {
     private static final Log LOG = LogFactory.getLog(TestActroArticleService.class);
+
+    @Test
+    public void test1() throws Exception {
+
+        assertEquals("apa", "apa");
+
+    }
 }
